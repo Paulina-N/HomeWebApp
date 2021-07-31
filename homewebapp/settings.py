f1e1e89c73d4e3221f4e15d7b296a1c127cb128c
Paulina Narkunaite <narkunaitepaulina@gmail.com>
2021-08-01 01:48:31 +0300

    update

--- homewebapp/settings.py
+++ homewebapp/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,6 +141,3 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
